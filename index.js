5f9a7bff997e1674f46ae2d0dc151803c11e7b53
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 00:45:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Write your code in this file!
 let currentUser = "This is the current user."
 
-const welcomeMessage = "Welcome to Flatbook." + currentUser + "!"
-
-
+const welcomeMessage = "Welcome to Flatbook," + currentUser + "!"
