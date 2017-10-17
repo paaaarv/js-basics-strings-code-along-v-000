40230464ff6e36673084170883cc42f25088b668
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 00:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let currentUser = "Parv Kumar";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}`
