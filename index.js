5b341a5972406e6e36b51a6b1ff465b508abbb15
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 00:50:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-let currentUser = "This is the current user."
+let currentUser = "Parv Kumar"
 
-const welcomeMessage = "Welcome to Flatbook, ${currentUser}!"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK ${currentUser}!"
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
